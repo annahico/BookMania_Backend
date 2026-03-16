7cee3c3efbf7d9db4ebc636d83df8ce6d3552b39
annahico <annahico@gmail.com>
2026-03-16 18:23:47 +0100

    feat(BOOK-9): implement JWT security configuration and user authentication (works I think)

---
+++ src/main/java/com/bookmania/bookmania/Repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.bookmania.bookmania.Repository;
+
+import com.bookmania.bookmania.Entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
